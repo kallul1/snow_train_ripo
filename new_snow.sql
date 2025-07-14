5932f63a9e0582c1c3c0c58576ca6ef13e4ddce0
Kallul Jyoti Nath <159731882+kallul1@users.noreply.github.com>
2025-07-14 16:42:21 +0530

    Update new_snow.sql

--- new_snow.sql
+++ new_snow.sql
@@ -1,19 +1,8 @@
 --database and warehouse
-CREATE DATABASE SF_DEMO;
 
-USE DATABASE SF_DEMO;
 
-CREATE SCHEMA SF_DEMO.DEMO;
-
-USE SCHEMA SF_DEMO.DEMO;
-
-CREATE WAREHOUSE DEMO_WH 
-  WAREHOUSE_TYPE = STANDARD 
-  WAREHOUSE_SIZE = XSMALL ;
-
----tables
 -- Permanent Table This is the default table type. Data persists until explicitly dropped.
-CREATE or replace TABLE employee (
+CREATE or replace TABLE new_employee(
     id INT,
     name STRING,
     department STRING,
